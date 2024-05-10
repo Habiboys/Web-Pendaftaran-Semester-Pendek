5dda6a0e03e47a84b985696f269d7eee1ef7017d
Muhammad Nouval Habibie <nouvalhabibie18@gmail.com>
2024-05-10 14:37:54 +0700

    revisi_auth

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -2,14 +2,12 @@ var express = require('express');
 var router = express.Router();
 const controller = require('../controller/auth.controller');
 const verifyToken= require ('../middleware/validtoken.middleware');
+const isLogin = require('../middleware/islogin.middleware');
 
 
 
 
-router.get('/login', controller.form);
+router.get('/login', isLogin, controller.form);
 router.post('/checklogin', controller.checklogin);
-
-
-
 router.post('/logout', verifyToken,controller.logout);
 module.exports = router;
