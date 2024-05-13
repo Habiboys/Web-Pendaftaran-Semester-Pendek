aa2541eb8d4dfd9bc7c657366b511f492848208b
ruchilamelinda <ruchilamelinda@gmail.com>
2024-05-13 09:48:29 +0700

    ubah password

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -3,11 +3,13 @@ var router = express.Router();
 const controller = require('../controller/auth.controller');
 const verifyToken= require ('../middleware/validtoken.middleware');
 const isLogin = require('../middleware/islogin.middleware');
-
+const passcontroller = require('../controller/ubahpasscontroller');
 
 
 
 router.get('/login', controller.form);
 router.post('/checklogin', controller.checklogin);
 router.post('/logout', verifyToken,controller.logout);
+router.post('/changepass', verifyToken, passcontroller.ubahpass);
+router.get('/ubahpassword', verifyToken, passcontroller.formchangepass)
 module.exports = router;
